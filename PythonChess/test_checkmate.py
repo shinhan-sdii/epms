df544f89790fb6193b972ad0c5cf29619d1e9ff8
shinhan-sdii <shinhan-sdii@shinhan.com>
2018-09-04 16:32:16 +0900

    test_checkmate.py 변경

--- PythonChess/test_checkmate.py
+++ PythonChess/test_checkmate.py
@@ -8,7 +8,7 @@ import unittest, mock
 import ChessRules 
 from ChessBoard import ChessBoard
 
-# PyChess test module
+# test module
 class CheckIsCheckmate(unittest.TestCase):
     "Class to unit check the IsCheckmate method of ChessRules.py module"
     # creating a mock for GetListOfValidMoves
@@ -17,19 +17,19 @@ class CheckIsCheckmate(unittest.TestCase):
         "Unit test for ChessRules method: IsCheckmate"
         # NOTE 1: We only check that there are valid moves for a given color
         # NOTE 2: We are NOT checking the setting color logic in this unit test. You need to write another unit test for this logic.
- 
+
         # Creating objects of Chessboard and ChessRules class and calling IsCheckmate function with each piece for initial position and "black" color
         cb = ChessBoard(0) #Create a chess board object with the initial position
         chess_rules_obj = ChessRules.ChessRules()
         chess_rules_obj.IsCheckmate(cb.GetState(),"black")
- 
+
         # Create expected_arg_calls list which is supposed to be called with GetListOfValidMoves for initial position
         # IsCheckmate calls GetListOfValidMoves with arguments: board, color (who is to play?) and co-ordinates of a square with a piece on it
         expected_arg_calls = []
         for row in range(0,2):
             for col in range(0,8):
                 expected_arg_calls.append(mock.call(cb.GetState(), 'black', (row, col)))
- 
+
         # Assert that the mockGetListOfValidMoves.call_args_list matches expected_arg_calls
         self.assertEqual(mockGetListOfValidMoves.call_args_list, expected_arg_calls)
 
@@ -38,15 +38,15 @@ class CheckIsCheckmate(unittest.TestCase):
 
         # check the number of times your mocked method was called
         self.assertEqual(mockGetListOfValidMoves.call_count,16)
- 
+
         # DID YOU KNOW?
         # assert_any_call can be used to assert a method that was called at least once with some arguments        
         #mockGetListOfValidMoves.assert_any_call(cb.GetState(),"black",(1,6))
- 
+
         # DID YOU KNOW?
         # call_count can be used to check the number of times your mocked method was called
         #self.assertEqual(mockGetListOfValidMoves.call_count,16)
- 
- 
+
+
 if __name__=="__main__":
     unittest.main()
